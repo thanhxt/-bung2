97c7bf8310300d7f4b46539b9776aff15a362a90
thanhxt <thanhtran_xuan@hotmail.de>
2019-10-31 16:27:12 +0100

    31.10.19
    check for boolean status(). not shure if its working and void gamestatus().

--- src/com/company/GameBoard.java
+++ src/com/company/GameBoard.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class GameBoard {
     private char [][] gameboard;
@@ -68,5 +69,78 @@ public class GameBoard {
     public boolean gameActive(){
         return ongoinggame;
     }
+
+    /**
+     * method will ask player for row and column, validate the inputs and call the makeMove()
+     * method.
+     * @param player
+     */
+    void askPlayer(char player){
+        Scanner sc = new Scanner(System.in);
+        int row, col;
+        do{
+            System.out.printf("Player %s Please enter a row (1-3): ", player);
+            row = sc.nextInt();
+
+            System.out.printf("Player %s Please enter a column (1-3): ", player);
+            col = sc.nextInt();
+        }
+        while(notValid(row,col));
+
+        makeMove(player,row-1,col-1);
+    }
+
+    /**
+     * this method will validate if the row and the column are between 1-3 and if the
+     * position is currently empty;
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean notValid(int row, int col){
+        if(row>3 || row<1 || col>3 || col<1 || !isEmpty(row, col))
+            return true;
+        return false;
+    }
+
+    /**
+     * this methos will check if a position is empty.
+     * @param row
+     * @param col
+     * @return
+     */
+    boolean isEmpty(int row, int col){
+        if(gameboard[row-1][col-1] == ' ')
+            return true;
+        else{
+            System.out.print("That position is taken \n");
+            return false;
+        }
+    }
+
+    /**
+     * NICHT FERTIG
+     * this method will check if there is a winner in between the rows or not
+     * @param row
+     * @param col
+     * @return
+     */
+    boolean status(int row, int col){
+        for(row=0;row<3;row++){
+            for(col=0;col<3;col++){
+                if(gameboard[row-1][col-1] == 'X' || gameboard[row-1][col-1] == 'O')
+                   return true;
+                else
+                    return false;
+            }
+        }
+    }
+    void gameStatus(char player){
+        if(status() = true)
+            System.out.printf( "%Player %s has won." , player );
+    }
+
+
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,21 @@ public class Main {
     public static void main(String[] args) {
         GameBoard tictac = new GameBoard();
 
+       // tictac.DisplayBoard();
+        //tictac.makeMove('O',0,1);
         tictac.DisplayBoard();
-        tictac.makeMove('O',0,1);
-        tictac.DisplayBoard();
+        int counter = 1;
+        while(tictac.gameActive()){
+            if(counter % 2 == 0)
+                tictac.askPlayer('X');
+            else
+                tictac.askPlayer('O');
+            counter++;
+            tictac.DisplayBoard();
+        }
+
+
+
     }
 
 
